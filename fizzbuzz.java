493abd9b55f9cf82c25b1437feb8c7c4611c3b22
Victor Castro II <victor.castro@mrjaffesclass.com>
2022-11-04 11:44:25 -0700

    fizzbuzz

---
+++ fizzbuzz.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class fizzbuzz here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class fizzbuzz
+{
+    public static void main(String[] args) {
+        int n = 100;
+        for (int i = 1; i <= n; i++) {
+            System.out.print(i + " "); 
+        }
+    }
+}
