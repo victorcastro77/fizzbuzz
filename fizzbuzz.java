a8b0d7b638f3c5b6be131e3111c849f2a81296cb
Victor-Castro <victor.castro@mrjaffesclass.com>
2022-11-11 12:37:11 -0800

    fizzbuzz

--- fizzbuzz.java
+++ fizzbuzz.java
@@ -11,14 +11,14 @@ public class fizzbuzz
         int a = 100;
         for (int i = 1; i <= a; i++) {
             System.out.println(i + " ");
-            if (i/3 == ) {
+            if (i / 5 == 0 && i / 3 == 0) {
+                System.out.println("fizzbuzz");
+            }
+            if ((i/5)==double(i/5)) {
                 System.out.println("fizz");
             }
-            if (i / 5 == 0) {
+            if (i / 3 == 0) {
                 System.out.println("buzz");
-            }
-            if (i / 15 == 0) {
-                System.out.println("fizzbuzz");
             }
         }
     }
