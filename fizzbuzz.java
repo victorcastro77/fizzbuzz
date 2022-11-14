758bc36d52659dabfe2b107e6e87efa344017821
Victor Castro II <victor.castro@mrjaffesclass.com>
2022-11-14 11:50:27 -0800

    fizzbuzz

--- fizzbuzz.java
+++ fizzbuzz.java
@@ -9,17 +9,20 @@ public class fizzbuzz
 {
     public static void main(String[] args) {
         int a = 100;
+        int fizz = 3;
+        int buzz = 5;
         for (int i = 1; i <= a; i++) {
             System.out.println(i + " ");
-            if (i / 5 == 0 && i / 3 == 0) {
+            boolean afizz = false;
+            boolean bbuzz = false; 
+            boolean cfizzbuzz = false;
+            if ((i/5) == (double)i/5 && (i/3) == (double)i/3) {
                 System.out.println("fizzbuzz");
-            }
-            if ((i/5)==double(i/5)) {
-                System.out.println("fizz");
-            }
-            if (i / 3 == 0) {
+            } else if ((i/5)==(double)i/5) {
                 System.out.println("buzz");
-            }
+            } else if ((i/3) == (double)i/3) {
+                System.out.println("fizz");
+            } 
         }
     }
 }
