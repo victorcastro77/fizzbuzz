3798d374c0c73877a88f2a0df826c6d18f5a65a8
Victor Castro II <victor.castro@mrjaffesclass.com>
2022-11-16 11:44:37 -0800

    fizzbuzz

--- fizzbuzz.java
+++ fizzbuzz.java
@@ -1,26 +1,21 @@
-
-/**
- * Write a description of class fizzbuzz here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
 public class fizzbuzz
 {
     public static void main(String[] args) {
         int counter = 0;
         for (int i = 1; i <= 100; i++) {
             counter = counter + 1;
-            int fizz = 3;
-            int buzz = 5;
-            if ((counter/5) == (double)counter/5 && (counter/3) == (double)counter/3) {
-                System.out.println("fizzbuzz");
-            } else if ((counter/5)==(double)counter/5) {
-                System.out.println("buzz");
-            } else if ((counter/3) == (double)counter/3) {
-                System.out.println("fizz");
-            } 
-            System.out.println(counter);
+            String fizz = "";
+            String buzz = "";
+            if ((i/5) == (double)i/5 && (i/3) == (double)i/3) {
+                fizz = "fizzbuzz";
+            } else if ((i/5) == (double)i/5) {
+                buzz = "buzz";
+            } else if ((i/3) == (double)i/3) {
+                fizz =  "fizz";
+            }
+            System.out.println(i+fizz+buzz);
+                fizz = "fizz";
+                buzz = "buzz";
         }
     }
 }
